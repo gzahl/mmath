a152d607903613a80e4fdbdd0fc7e94480eee2b5
Manuel Jung <manuel.jung@hotmail.com>
2021-07-03 12:32:42 +0200

    Add norm etc.

--- include/mmath/Quaternion.h
+++ include/mmath/Quaternion.h
@@ -29,14 +29,16 @@
 #ifndef MMATH_QUATERNION_H_
 #define MMATH_QUATERNION_H_
 
+#include <cstdint>
+
 #include "Vector3.h"
 #include "util.h"
-#include <cstdint>
 
 namespace mmath {
 
-template <typename T> class Quaternion {
-public:
+template <typename T>
+class Quaternion {
+ public:
   union {
     T buf[4];
     struct {
@@ -47,16 +49,21 @@ public:
   constexpr Quaternion();
   constexpr Quaternion(const Quaternion &v);
   constexpr Quaternion(T w, T x, T y, T z);
+  constexpr Quaternion(const Vector<3, T> axis, const T angle);
 
   constexpr Vector<3, T> ToEulerXYZ();
   constexpr void FromEulerXYZ(const Vector<3, T> &e);
   constexpr T Dot(const Quaternion &q);
   constexpr T Angle(const Quaternion &q);
+  constexpr T norm();
 };
 
 template <typename T>
 constexpr Quaternion<T> operator*(const Quaternion<T> &q1,
                                   const Quaternion<T> &q2);
+
+template <typename T>
+constexpr Quaternion<T> operator/(const Quaternion<T> &q1, const T &scalar);
 
 template <typename T>
 constexpr Quaternion<T>::Quaternion() : w(1), x(0), y(0), z(0) {}
@@ -65,11 +72,23 @@ template <typename T>
 constexpr Quaternion<T>::Quaternion(const Quaternion &v)
     : w(v.w), x(v.x), y(v.y), z(v.z) {}
 
+/**
+ * q = cos(a/2) + i ( x * sin(a/2)) + j (y * sin(a/2)) + k ( z * sin(a/2))
+ * https://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/index.htm
+ */
+template <typename T>
+constexpr Quaternion<T>::Quaternion(const Vector<3, T> axis, const T angle)
+    : w(cos(angle / 2.)),
+      x(axis.x * sin(angle / 2.)),
+      y(axis.y * sin(angle / 2.)),
+      z(axis.z * sin(angle / 2.)) {}
+
 template <typename T>
 constexpr Quaternion<T>::Quaternion(T w, T x, T y, T z)
     : w(w), x(x), y(y), z(z) {}
 
-template <typename T> constexpr Vector<3, T> Quaternion<T>::ToEulerXYZ() {
+template <typename T>
+constexpr Vector<3, T> Quaternion<T>::ToEulerXYZ() {
   T ex = 0.0, ey = 0.0, ez = 0.0;
 
   T x2 = this->x + this->x;
@@ -89,13 +108,13 @@ template <typename T> constexpr Vector<3, T> Quaternion<T>::ToEulerXYZ() {
   T sy = 1;
   T sz = 1;
 
-  T m11 = (1 - (yy + zz)) * sx; // Matrix4[0][0] (Matrix4.Element[0])
-  T m12 = (xy - wz) * sy;       // Matrix4[1][0] (Matrix4.Element[4])
-  T m13 = (xz + wy) * sz;       // Matrix4[2][0] (Matrix4.Element[8])
-  T m22 = (1 - (xx + zz)) * sy; // Matrix4[1][1] (Matrix4.Element[5])
-  T m23 = (yz - wx) * sz;       // Matrix4[2][1] (Matrix4.Element[9])
-  T m32 = (yz + wx) * sy;       // Matrix4[1][2] (Matrix4.Element[6])
-  T m33 = (1 - (xx + yy)) * sz; // Matrix4[2][2] (Matrix4.Element[10])
+  T m11 = (1 - (yy + zz)) * sx;  // Matrix4[0][0] (Matrix4.Element[0])
+  T m12 = (xy - wz) * sy;        // Matrix4[1][0] (Matrix4.Element[4])
+  T m13 = (xz + wy) * sz;        // Matrix4[2][0] (Matrix4.Element[8])
+  T m22 = (1 - (xx + zz)) * sy;  // Matrix4[1][1] (Matrix4.Element[5])
+  T m23 = (yz - wx) * sz;        // Matrix4[2][1] (Matrix4.Element[9])
+  T m32 = (yz + wx) * sy;        // Matrix4[1][2] (Matrix4.Element[6])
+  T m33 = (1 - (xx + yy)) * sz;  // Matrix4[2][2] (Matrix4.Element[10])
 
   ey = asin(clamp(m13, -1, 1));
 
@@ -126,24 +145,37 @@ constexpr void Quaternion<T>::FromEulerXYZ(const Vector<3, T> &e) {
   w = c1 * c2 * c3 - s1 * s2 * s3;
 }
 
-template <typename T> constexpr T Quaternion<T>::Dot(const Quaternion<T> &q) {
+template <typename T>
+constexpr T Quaternion<T>::Dot(const Quaternion<T> &q) {
   Quaternion<T> qt = *this * q;
   return qt.w + qt.x + qt.y + qt.z;
 }
 
-template <typename T> constexpr T Quaternion<T>::Angle(const Quaternion<T> &q) {
+template <typename T>
+constexpr T Quaternion<T>::Angle(const Quaternion<T> &q) {
   return acos(abs(clamp(Dot(q), -1.0, 1.0))) * 2.0;
+}
+
+template <typename T>
+constexpr T Quaternion<T>::norm() {
+  return sqrt(this->w * this->w + this->x * this->x + this->y * this->y +
+              this->z * this->z);
 }
 
 template <typename T>
 constexpr Quaternion<T> operator*(const Quaternion<T> &q1,
                                   const Quaternion<T> &q2) {
   Quaternion<T> ret;
-  ret.a = q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z;
-  ret.b = q1.x * q2.w + q1.w * q2.x + q1.y * q2.z - q1.z * q2.y;
-  ret.c = q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x;
-  ret.d = q1.w * q2.z + q1.x * q2.y - q1.y * q2.x + q1.z * q2.w;
+  ret.w = q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z;
+  ret.x = q1.x * q2.w + q1.w * q2.x + q1.y * q2.z - q1.z * q2.y;
+  ret.y = q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x;
+  ret.z = q1.w * q2.z + q1.x * q2.y - q1.y * q2.x + q1.z * q2.w;
   return ret;
+}
+
+template <typename T>
+constexpr Quaternion<T> operator/(const Quaternion<T> &q, const T &scalar) {
+  return Quaternion<T>(q.w / scalar, q.x / scalar, q.y / scalar, q.z / scalar);
 }
 
 using QuaternionF = Quaternion<float>;
@@ -152,6 +184,6 @@ using Quat = Quaternion<float>;
 using QuaternionLF = Quaternion<double>;
 using Quat64 = Quaternion<double>;
 
-} // namespace mmath
+}  // namespace mmath
 
 #endif
